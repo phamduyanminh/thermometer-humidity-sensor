9a71a20e6a067fb7d30e86c7ed3a8fce063cdc06
Minh Pham <phamduya@sheridancollege.ca>
2025-06-01 15:16:03 -0400

    Retrieving data from the txt file

--- thermometers_humidity_sensors.py
+++ thermometers_humidity_sensors.py
@@ -5,8 +5,35 @@ def thermometer_humidity_sensors(log_file):
     if not lines:
         print("Log is empty")
         return
-    else:
-        print(lines)
+    
+    
+    reference_temperature = None
+    reference_humidity = None
+    sensor_data = {}
+    sensor_data_order = []
+    result_output = []
+    
+    reference_line = lines[0].split()
+    reference_temperature = float(reference_line[1])
+    reference_humidity = float(reference_line[2])
+    
+    for line in lines[1:]:
+        data = line.strip()
+        data_section = data.split()
+        
+        if data_section[0] == "thermometer" or data_section[0] == "humidity":
+            sensor_type = data_section[0]
+            sensor_name = data_section[1]
+
+            if sensor_name not in sensor_data:
+                sensor_data[sensor_name] = {"type": sensor_type, "datas": []}
+                sensor_data_order.append(sensor_name)
+        else:
+            sensor_name = data_section[1]
+            sensor_value = float(data_section[2])
+            
+            if sensor_name in sensor_data:
+                sensor_data[sensor_name]["datas"].append(sensor_value)
                 
 
 log_file = "log.txt"
