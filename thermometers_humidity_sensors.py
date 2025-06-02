3a20f5721f3a21b0e55e09b67bb128ab6cf43519
Minh Pham <phamduya@sheridancollege.ca>
2025-06-02 11:23:53 -0400

    Improve retrieving data from log txt fil code

--- thermometers_humidity_sensors.py
+++ thermometers_humidity_sensors.py
@@ -1,3 +1,13 @@
+class Sensor:
+    def __init__(self, name, type):
+        self.name = name
+        self.type = type
+        self.datas = []
+    
+    def set_datas(self, value):
+        self.datas.append(value)
+        
+
 def thermometer_humidity_sensors(log_file):
     lines = open(log_file, "r").readlines()
     
@@ -6,34 +16,31 @@ def thermometer_humidity_sensors(log_file):
         print("Log is empty")
         return
     
-    
-    reference_temperature = None
-    reference_humidity = None
-    sensor_data = {}
-    sensor_data_order = []
-    result_output = []
+    sensor_map = {}
+    sensor_order = []
+    sensor_result_output = []
     
     reference_line = lines[0].split()
-    reference_temperature = float(reference_line[1])
+    reference_thermometer = float(reference_line[1])
     reference_humidity = float(reference_line[2])
     
     for line in lines[1:]:
-        data = line.strip()
-        data_section = data.split()
+        line_data = line.strip()
+        data_section = line_data.split()
         
         if data_section[0] == "thermometer" or data_section[0] == "humidity":
             sensor_type = data_section[0]
             sensor_name = data_section[1]
-
-            if sensor_name not in sensor_data:
-                sensor_data[sensor_name] = {"type": sensor_type, "datas": []}
-                sensor_data_order.append(sensor_name)
+            
+            if sensor_name not in sensor_map:
+                sensor_map[sensor_name] = Sensor(sensor_name, sensor_type)
+                sensor_order.append(sensor_name)
         else:
             sensor_name = data_section[1]
             sensor_value = float(data_section[2])
-            
-            if sensor_name in sensor_data:
-                sensor_data[sensor_name]["datas"].append(sensor_value)
+            if sensor_name in sensor_map:
+                sensor_object = sensor_map[sensor_name]
+                sensor_object.set_datas(sensor_value)
                 
 
 log_file = "log.txt"
